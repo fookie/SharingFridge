bad380e0294b43034cf96cdeaf83f0dc594b0f6d
Paulay Liu <liubaoleiengineering2@emails.bjut.edu.cn>
2016-10-12 12:32:21 +0100

    Revert "navigation bar trial 1."
    This reverts commit b24a1ad9a1c710b048084e7a65165521a6da6c15.

---
+++ app/src/main/java/android/assignment/sharingfridge/MainActivity.java
@@ -0,0 +1,17 @@
+package android.assignment.sharingfridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //hehe
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Comment by LIn
+        // 233
+        //sdfdsf
+    }
+}
