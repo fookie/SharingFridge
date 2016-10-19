95f2c4a32bdcf1281a3703b7a44e8bfe926d28f5
26space <26sp330@gmail.com>
2016-10-19 14:36:38 +0100

    little increment

--- app/src/main/java/android/assignment/sharingfridge/HomeActivity.java
+++ app/src/main/java/android/assignment/sharingfridge/HomeActivity.java
@@ -108,7 +108,8 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+
             return true;
         }
 
