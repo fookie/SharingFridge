22ffac1259861cd56b8f1f411a31d826cee5470a
Hale <mahongzhi009@gmail.com>
2016-11-14 17:45:26 +0000

    basic join/create group
    unfinish

--- app/src/main/java/android/assignment/sharingfridge/MemberFragment.java
+++ app/src/main/java/android/assignment/sharingfridge/MemberFragment.java
@@ -1,18 +1,34 @@
 package android.assignment.sharingfridge;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Canvas;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,6 +49,13 @@ public class MemberFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    //stuffs that join layout would use
+    private Button submit=null;
+    private SendRequestTask mAuthTask = null;
+    private EditText groupname;
+    private RadioButton joingroup;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -84,6 +107,11 @@ public class MemberFragment extends Fragment {
         View view=null;
         if(UserStatus.hasLogin&&!UserStatus.inGroup){
             view = inflater.inflate(R.layout.join_group_layout, container, false);
+            submit=(Button)view.findViewById(R.id.join_group_submit);
+            groupname=(EditText)view.findViewById(R.id.groupname_edittext);
+            joingroup=(RadioButton)view.findViewById(R.id.join_group_radio);
+            submit.setOnClickListener(new submitClick());
+
         }else {
             view = inflater.inflate(R.layout.fragment_member, container, false);
             RecyclerView memberView = (RecyclerView) view.findViewById(R.id.memberView);
@@ -152,6 +180,92 @@ public class MemberFragment extends Fragment {
             memberItems.add(new MemberItem(owner, count + getString(R.string.group_sta_hint) + amount, owner + ".png"));
         }
         return memberItems;
+    }
+
+    private class submitClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            mAuthTask =new SendRequestTask(joingroup.isChecked()?"join":"create",groupname.getText().toString());
+            mAuthTask.execute();
+        }
+    }
+
+    private class SendRequestTask extends AsyncTask<String, Void, String> {
+        private String urlString = "http://178.62.93.103/SharingFridge/group.php";
+        private String action,groupname;
+        public SendRequestTask(String _action, String _groupname) {
+            action=_action;
+            groupname=_groupname;
+        }
+
+        protected String doInBackground(String... params) {
+            return performPostCall();
+        }
+
+        public String performPostCall() {
+            Log.d("send post", "performPostCall");
+            String response = "";
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);/* milliseconds */
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                //conn.setRequestProperty("Content-Type", "application/json");
+                //make json object
+                JSONObject jo = new JSONObject();
+                jo.put("action", action);
+                jo.put("username", UserStatus.username);
+                jo.put("groupname",groupname);
+                String tosend = jo.toString();
+                Log.d("JSON", tosend);
+
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+                outputStreamWriter.write("group=" + tosend);
+                outputStreamWriter.flush();
+                outputStreamWriter.close();
+
+                int responseCode = conn.getResponseCode();
+
+                InputStream inputStream = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                int length = 500;
+                String contentAsString = convertInputStreamToString(inputStream, length);
+                return contentAsString;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        public String convertInputStreamToString(InputStream stream, int length) throws IOException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[length];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mAuthTask = null;
+            String permission;
+            try {
+                JSONObject confirm = new JSONObject(result);
+                permission = confirm.get("permission").toString();
+                if (permission.equals("granted")) {
+                    Log.d("GROUP", action+" SUCCESS");
+                    UserStatus.inGroup=true;
+                    UserStatus.groupName=groupname;
+                }
+            } catch (JSONException je) {
+                je.printStackTrace();
+                Log.d("GROUP",action+ "FAILED");
+            }
+        }
     }
 
 }
