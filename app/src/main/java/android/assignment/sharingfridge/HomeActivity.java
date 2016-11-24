2d222ccb1007a8d3a7e09806e71cd8508f3f0eea
Paulay Liu <liubaoleiengineering2@emails.bjut.edu.cn>
2016-11-24 23:00:32 +0000

    Conflict resolved

--- app/src/main/java/android/assignment/sharingfridge/HomeActivity.java
+++ app/src/main/java/android/assignment/sharingfridge/HomeActivity.java
@@ -32,7 +32,6 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -139,7 +138,7 @@ public class HomeActivity extends AppCompatActivity
             UserStatus.hasLogin = true;
             UserStatus.inGroup = !UserStatus.groupName.equals("none");
         }
-        Log.d("CHAT-TOKEN",UserStatus.token);
+//        Log.d("CHAT-TOKEN",UserStatus.token);
         if(UserStatus.token!=null&&!UserStatus.token.equals("")) {
             RongIM.getInstance().setCurrentUserInfo(findUserById(UserStatus.username));
             RongIM.getInstance().setMessageAttachedUserInfo(true);
